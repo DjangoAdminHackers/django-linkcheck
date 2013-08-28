a6bc79ae9558fe337dd1cf0c3f2c1ed2e9028fc2
fruitschen <fruitschen@gmail.com>
2013-08-28 09:37:34 +0800

    new style urls import for django1.6

--- linkcheck/urls.py
+++ linkcheck/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import *
+except:
+    from django.conf.urls import *
 
 urlpatterns = patterns('linkcheck.views',
    (r'^coverage/$', 'coverage'),
