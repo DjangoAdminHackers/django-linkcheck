544c88637fa996c797a95be4e798fe6a39e8117d
Andy Baker <andy@andybak.net>
2016-04-25 12:30:24 +0100

    unused import

--- linkcheck/views.py
+++ linkcheck/views.py
@@ -10,7 +10,6 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse, NoReverseMatch
-from django.db import models
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
