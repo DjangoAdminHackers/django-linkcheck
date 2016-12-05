61748e100472a40bab17b6f0e87eb5c95c2def96
Moritz Duchêne <duchene@team23.de>
2016-12-05 15:34:08 +0100

    rename south_migrations/ to migrations/

--- linkcheck/migrations/0001_initial.py
+++ linkcheck/migrations/0001_initial.py
@@ -1,41 +1,72 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
 
-from django.db import models, migrations
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Url'
+        db.create_table(u'linkcheck_url', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
+            ('last_checked', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('status', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
+            ('message', self.gf('django.db.models.fields.CharField')(max_length=1024, null=True, blank=True)),
+            ('still_exists', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'linkcheck', ['Url'])
+
+        # Adding model 'Link'
+        db.create_table(u'linkcheck_link', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('field', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('url', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'links', to=orm['linkcheck.Url'])),
+            ('text', self.gf('django.db.models.fields.CharField')(default=u'', max_length=256)),
+            ('ignore', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'linkcheck', ['Link'])
 
 
-class Migration(migrations.Migration):
+    def backwards(self, orm):
+        # Deleting model 'Url'
+        db.delete_table(u'linkcheck_url')
 
-    dependencies = [
-        ('contenttypes', '0001_initial'),
-    ]
+        # Deleting model 'Link'
+        db.delete_table(u'linkcheck_link')
 
-    operations = [
-        migrations.CreateModel(
-            name='Link',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('object_id', models.PositiveIntegerField()),
-                ('field', models.CharField(max_length=128)),
-                ('text', models.CharField(default='', max_length=256)),
-                ('ignore', models.BooleanField(default=False)),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType', on_delete=models.CASCADE)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Url',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('url', models.CharField(unique=True, max_length=255)),
-                ('last_checked', models.DateTimeField(null=True, blank=True)),
-                ('status', models.NullBooleanField()),
-                ('message', models.CharField(max_length=1024, null=True, blank=True)),
-                ('still_exists', models.BooleanField(default=False)),
-            ],
-        ),
-        migrations.AddField(
-            model_name='link',
-            name='url',
-            field=models.ForeignKey(related_name='links', to='linkcheck.Url', on_delete=models.CASCADE),
-        ),
-    ]
+
+    models = {
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'linkcheck.link': {
+            'Meta': {'object_name': 'Link'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            'field': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ignore': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'text': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '256'}),
+            'url': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'links'", 'to': u"orm['linkcheck.Url']"})
+        },
+        u'linkcheck.url': {
+            'Meta': {'object_name': 'Url'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_checked': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'still_exists': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'url': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['linkcheck']
