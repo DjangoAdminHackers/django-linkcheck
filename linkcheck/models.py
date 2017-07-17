5bcc19944cfa2f3989298c123686e20ad35e5b6c
andybak <andy@andybak.net>
2017-07-17 13:47:39 +0100

    Truncate redirect urls. It's not idea but it's better than failing to save the entire instance. See issue #75

--- linkcheck/models.py
+++ linkcheck/models.py
@@ -359,7 +359,9 @@ class Url(models.Model):
             self.message = 'Other Error: %s' % e
         else:
             if response.getcode() == 301 and response.geturl() != url:
-                self.redirect_to = response.geturl()
+                redirect_url = response.geturl()
+                redirect_url = redirect_url[:MAX_REDIRECT_URL_LENGTH]
+                self.redirect_to = redirect_url
             elif self.redirect_to:
                 self.redirect_to = ''
 
