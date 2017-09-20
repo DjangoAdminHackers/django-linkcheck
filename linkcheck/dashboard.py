e32e5a41dc1cc549a94bd6a1405122cf22c30a65
andybak <andy@andybak.net>
2017-09-20 09:11:06 +0100

    The dashboard stuff was getting too specific to our codebase and doesn't really belong here. I might publish it as a separate project and deprecate this dashboard module.

--- linkcheck/dashboard.py
+++ linkcheck/dashboard.py
@@ -1,4 +1,4 @@
-from .dashboard_extra_modules import PermCheckingLinkList
+from admin_tools.dashboard import modules
 from linkcheck.views import get_status_message
 
 try:
@@ -7,7 +7,7 @@ except ImportError:  # Django < 1.10
     from django.core.urlresolvers import reverse
 
 
-linkcheck_dashboard_module = PermCheckingLinkList(
+linkcheck_dashboard_module = modules.LinkList(
     title="Linkchecker",
     pre_content=get_status_message,
     children=(
@@ -15,6 +15,5 @@ linkcheck_dashboard_module = PermCheckingLinkList(
         {'title': 'Broken links', 'url': reverse('linkcheck_report')},
         {'title': 'Untested links', 'url': reverse('linkcheck_report') + '?filters=show_unchecked'},
         {'title': 'Ignored links', 'url': reverse('linkcheck_report') + '?filters=ignored'},
-    ),
-    required_perms=['linkcheck.can_change_link'],
+    )
 )
