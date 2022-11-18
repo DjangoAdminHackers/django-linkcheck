93e5521d210123efc40b7172cca9f47944ba14b8
Timo Ludwig <ti.ludwig@web.de>
2022-11-18 09:16:58 +0100

    Remove unused field `still_exists` from `Url` model

---
+++ linkcheck/migrations/0004_remove_url_still_exists.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('linkcheck', '0003_redirect_to_as_textfield'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='url',
+            name='still_exists',
+        ),
+    ]
