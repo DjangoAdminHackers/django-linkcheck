8783a31b084d8c01100048aa5dee7e4717845884
Timo Ludwig <ti.ludwig@web.de>
2022-11-19 23:35:30 +0100

    Add Django framework classifiers

--- setup.py
+++ setup.py
@@ -45,5 +45,8 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Framework :: Django',
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
     ],
 )
