64d43a709246a96a2ea95302b8b9f536df216109
Ben Stockermans <ben.stockermans@beauhurst.com>
2025-07-29 13:24:14 +0100

    Added environment option to skip compilation of translation files.

--- linkcheck/build_meta.py
+++ linkcheck/build_meta.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 from setuptools import build_meta as default
@@ -5,17 +6,29 @@ from setuptools.build_meta import *  # noqa: F401, F403
 
 
 def compile_translation_files():
-    print("Compile translation files")
+    print("Compiling translation files...")
     subprocess.run(["django-admin", "compilemessages"], cwd="linkcheck")
 
 
+def should_compile_translation_files():
+    skip_translations = os.environ.get("LINKCHECK_SKIP_TRANSLATIONS")
+    if skip_translations and skip_translations.lower() in ("1", "true", "yes", "t", "y"):
+        return False
+
+    return True
+
+
 def build_sdist(sdist_directory, config_settings=None):
-    compile_translation_files()
+    if should_compile_translation_files():
+        compile_translation_files()
+
     return default.build_sdist(sdist_directory, config_settings)
 
 
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
-    compile_translation_files()
+    if should_compile_translation_files():
+        compile_translation_files()
+
     return default.build_wheel(
         wheel_directory,
         config_settings=config_settings,
