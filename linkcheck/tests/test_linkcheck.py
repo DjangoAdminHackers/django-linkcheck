5c3218dddd73ffeb42fd9f19c0de43197e9e8643
Ben Stockermans <ben.stockermans@beauhurst.com>
2025-09-18 16:23:08 +0100

    Removed Mock import.

--- linkcheck/tests/test_linkcheck.py
+++ linkcheck/tests/test_linkcheck.py
@@ -2,7 +2,7 @@ import os
 import sys
 from datetime import datetime, timedelta
 from io import StringIO
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import requests_mock
 import urllib3
